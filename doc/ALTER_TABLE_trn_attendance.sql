63e506b5e54e9f4bb375fdc8fe15f1dcfe028c5d
masak <oicwam_m-Suda@users.noreply.github.com>
2018-11-18 23:02:33 +0900

    【Pinoco】pinocoのテーブル追加SQLをMySQL仕様に修正

--- doc/ALTER_TABLE_trn_attendance.sql
+++ doc/ALTER_TABLE_trn_attendance.sql
@@ -1,35 +1,20 @@
--- Table: trn_attendance
-
--- DROP TABLE trn_attendance;
-
-CREATE TABLE trn_attendance
+-- auto-generated definition
+create table trn_attendance
 (
-  id serial NOT NULL, -- 勤怠ID
-  user_id character varying(6) NOT NULL, --ユーザーID
-  today character varying(8), -- 本日日付
-  arrival_time character varying(14), -- 出勤時間
-  leave_time character varying(14), -- 退勤時間
-  day_report text, -- 日報
-  create_user character varying(16) NOT NULL, -- 登録者
-  create_date character varying(14) NOT NULL, -- 登録日時
-  update_user character varying(16) NOT NULL, -- 更新者
-  update_date character varying(14) NOT NULL, -- 更新日時
-  is_delete smallint NOT NULL DEFAULT 0, -- 削除フラグ
-  CONSTRAINT trn_attendance_pkey PRIMARY KEY (id)
-)
-WITH (
-  OIDS=FALSE
+  id           bigint unsigned auto_increment,
+  user_id      varchar(50) charset latin1 not null,
+  today        varchar(8) charset latin1  null,
+  arrival_time varchar(14) charset latin1 null,
+  leave_time   varchar(14) charset latin1 null,
+  day_report   text charset latin1        null,
+  create_user  varchar(16) charset latin1 not null,
+  create_date  varchar(14) charset latin1 not null,
+  update_user  varchar(16) charset latin1 not null,
+  update_date  varchar(14) charset latin1 not null,
+  is_delete    smallint(6) default '0'    not null,
+  constraint id
+  unique (id)
 );
-ALTER TABLE trn_attendance
-  OWNER TO postgres;
-COMMENT ON COLUMN trn_attendance.id IS '勤怠ID';
-COMMENT ON COLUMN trn_attendance.user_id IS 'ユーザーID';
-COMMENT ON COLUMN trn_attendance.today IS '本日日付';
-COMMENT ON COLUMN trn_attendance.arrival_time IS '出勤時間';
-COMMENT ON COLUMN trn_attendance.leave_time IS '退勤時間';
-COMMENT ON COLUMN trn_attendance.day_report IS '日報';
-COMMENT ON COLUMN trn_attendance.create_date IS '登録日時';
-COMMENT ON COLUMN trn_attendance.update_user IS '更新者';
-COMMENT ON COLUMN trn_attendance.update_date IS '更新日時';
-COMMENT ON COLUMN trn_attendance.is_delete IS '削除フラグ';
 
+alter table trn_attendance
+  add primary key (id);
