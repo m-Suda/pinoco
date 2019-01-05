453e907404ee48ae413ffb7a5996e311e19acea5
masak <oicwam.orz1228@gmail.com>
2019-01-05 10:41:07 +0900

    テーブル作成SQLの文字コードを修正

--- doc/ALTER_TABLE_mst_company.sql
+++ doc/ALTER_TABLE_mst_company.sql
@@ -1,12 +1,18 @@
 -- auto-generated definition
 create table mst_company
 (
-  company_id   varchar(32)                not null
+  company_id        varchar(32)             not null
     primary key,
-  company_name varchar(64) charset latin1 null,
-  create_user  varchar(16) charset latin1 not null,
-  create_date  varchar(14) charset latin1 not null,
-  update_user  varchar(16) charset latin1 not null,
-  update_date  varchar(14) charset latin1 not null,
-  is_delete    smallint(6) default '0'    not null
+  company_name      varchar(64)             null,
+  create_user       varchar(16)             not null,
+  create_date       varchar(14)             not null,
+  update_user       varchar(16)             not null,
+  update_date       varchar(14)             not null,
+  is_delete         smallint(6) default '0' not null,
+  tel               varchar(12)             null,
+  email             text                    null,
+  company_name_kana varchar(128)            null,
+  zip               varchar(8)              null,
+  address           text                    null
 );
+
